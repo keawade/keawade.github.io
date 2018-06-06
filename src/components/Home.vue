506a7bdc4781d132adc1501a3f8ebefca51c5546
Keith Wade <keawade@protonmail.com>
2018-06-06 08:34:29 -0500

    Add dates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="home">
     <h1>Articles and Projects</h1>
-    <div v-for='post in posts' class='article'>
-      <h2 class='header'><router-link :to='post.filename' class=''>{{post.title}}</router-link></h2>
-      <div>{{post.excerpt}}</div>
+    <div v-for='post in posts' :key='{post}' class='article'>
+      <div class='title'>
+        <h2 class='header'><router-link :to='post.filename' class=''>{{post.title}}</router-link></h2>
+        <div class='date'>{{post.date}}</div>
+      </div>
+      <div class='excerpt'>{{post.excerpt}}</div>
     </div>
   </div>
 </template>
@@ -11,12 +14,18 @@
 <script>
 import { posts } from '../posts/posts.json'
 import '../assets/list.min.css'
+import moment from 'moment'
 
 export default {
   name: 'home',
   data () {
     return {
-      posts: posts.sort((a, b) => (Date.parse(a.date) < Date.parse(b.date)))
+      posts: posts
+        .sort((a, b) => (Date.parse(a.date) < Date.parse(b.date)))
+        .map((post) => {
+          post.date = moment(post.date).format('LL')
+          return post
+        })
     }
   }
 }
@@ -24,12 +33,27 @@ export default {
 
 <style lang='scss' scoped>
 #home {
-    .article {
-      padding-bottom: 1rem;
-    }
+  .article {
+    padding-bottom: 1rem;
+  }
   .header {
     a {
       text-decoration: none;
+    }
+  }
+  .title {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 0.67rem;
+    // justify-content: space-between;
+    // align-items: center;
+
+    h2 {
+      margin-bottom: 0.25rem;
+    }
+
+    .date {
+      font-size: 0.75rem;
     }
   }
 }
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,6 +1,9 @@
 <template>
   <div id='post'>
-    <h1>{{metadata.title}}</h1>
+    <div class='title'>
+      <h1>{{metadata.title}}</h1>
+      <div class='date'>{{metadata.date}}</div>
+    </div>
     <div v-html='content'></div>
   </div>
 </template>
@@ -9,6 +12,7 @@
 import { posts } from '../posts/posts.json'
 import marked from 'marked'
 import hljs from 'highlight.js'
+import moment from 'moment'
 
 marked.setOptions({
   highlight: function (code) {
@@ -19,13 +23,30 @@ marked.setOptions({
 export default {
   name: 'post',
   data () {
+    const metadata = posts.filter((post) => (post.filename === this.$route.params.name)).pop()
+    metadata.date = moment(metadata.date).format('LL')
     return {
       content: marked(require(`../posts/${this.$route.params.name}.md`)),
-      metadata: posts.filter((post) => (post.filename === this.$route.params.name)).pop()
+      metadata: metadata
     }
   }
 }
 </script>
 
 <style lang='scss' scoped>
+  .title {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 0.67rem;
+    // justify-content: space-between;
+    // align-items: center;
+
+    h1 {
+      margin-bottom: 0.25rem;
+    }
+
+    .date {
+      font-size: 0.75rem;
+    }
+  }
 </style>
