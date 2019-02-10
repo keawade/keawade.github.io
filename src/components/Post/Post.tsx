8342e081579d35b8f736a85afe07b6e3fd294ab1
Keith Wade <keawade@protonmail.com>
2019-02-10 09:18:24 -0600

    Markdown handling for posts

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -3,14 +3,68 @@ import { RouteComponentProps } from 'react-router-dom';
 
 import './Post.scss';
 
-import { Segment } from 'semantic-ui-react';
+import Markdown from 'react-markdown';
+import { Loader, Segment } from 'semantic-ui-react';
+import { FourOhFour } from '../FourOhFour';
 
 interface IPostProps extends RouteComponentProps<{ post: string }> {}
 
-export const Post: React.FunctionComponent<IPostProps> = ({
-  history,
-  location,
-  match,
-}) => {
-  return <Segment>{match.params.post}</Segment>;
-};
+interface IPostState {
+  markdownSource?: string;
+  error?: string;
+}
+
+export class Post extends React.Component<IPostProps, IPostState> {
+  constructor(props: IPostProps) {
+    super(props);
+
+    this.state = {
+      markdownSource: undefined,
+      error: undefined,
+    };
+  }
+
+  public async componentDidMount() {
+    let markdownSource: string | undefined;
+    try {
+      markdownSource = await this.fetchMarkdown(
+        require(`../../posts/${this.props.match.params.post}.md`),
+      );
+    } catch (err) {
+      // Swallow error.
+    }
+    this.setState(prevState => ({
+      ...prevState,
+      markdownSource,
+      error: !markdownSource ? 'Failed to load article.' : undefined,
+    }));
+  }
+
+  private fetchMarkdown = async (path: string) => {
+    const response = await fetch(path);
+    if (response.ok) {
+      return response.text();
+    }
+    return undefined;
+  };
+
+  public render() {
+    if (!this.state.markdownSource && !this.state.error) {
+      return (
+        <Segment>
+          <Loader />
+        </Segment>
+      );
+    }
+
+    if (this.state.error) {
+      return <FourOhFour {...this.props} />;
+    }
+
+    return (
+      <Segment>
+        <Markdown source={this.state.markdownSource} />
+      </Segment>
+    );
+  }
+}
