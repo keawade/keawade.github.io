473771f0a81a0b2fee4dc9de791c935ca8facd9f
Keith Wade <keawade@protonmail.com>
2019-02-10 21:02:59 -0600

    Add about page content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,8 +3,59 @@ import { RouteComponentProps } from 'react-router-dom';
 
 import './About.scss';
 
-import { Segment } from 'semantic-ui-react';
+import Markdown from 'react-markdown';
+import { Loader, Segment } from 'semantic-ui-react';
 
-export const About: React.FunctionComponent<RouteComponentProps> = props => {
-  return <Segment>About Page</Segment>;
-};
+interface IAboutState {
+  markdownSource?: string;
+}
+
+export class About extends React.Component<RouteComponentProps, IAboutState> {
+  constructor(props: RouteComponentProps) {
+    super(props);
+
+    this.state = {
+      markdownSource: undefined,
+    };
+  }
+
+  public async componentDidMount() {
+    let markdownSource: string | undefined;
+    try {
+      markdownSource = await this.fetchMarkdown(require(`./about.md`));
+    } catch (err) {
+      // Swallow error.
+    }
+    this.setState(prevState => ({
+      ...prevState,
+      markdownSource,
+    }));
+  }
+
+  private fetchMarkdown = async (path: string) => {
+    const response = await fetch(path);
+    if (response.ok) {
+      return response.text();
+    }
+    return undefined;
+  };
+
+  public render() {
+    if (!this.state.markdownSource) {
+      return (
+        <Segment id='about'>
+          <Loader />
+        </Segment>
+      );
+    }
+
+    return (
+      <Segment id='about'>
+        <Markdown
+          source={this.state.markdownSource}
+          // renderers={{ code: CodeBlock }}
+        />
+      </Segment>
+    );
+  }
+}
